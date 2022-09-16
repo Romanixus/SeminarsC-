0654a4c2c3834cad8b3e2e3d1626dd7886df2d70
Ogara <ya.rom4345@yandex.ru>
2022-09-16 20:59:26 +0300

    Задача 8

---
+++ Seminar1/Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.*/
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+int num = 1;
+Console.Write($"Чётные числа от 1 до {N}: ");
+while (num <= N)
+{
+    if (num % 2 == 0)
+    {
+        Console.Write($"{num} ");
+    }
+    num++;
+}
