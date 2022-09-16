9825ef0bd98ebc608c268d2b955f537292284747
Ogara <ya.rom4345@yandex.ru>
2022-09-16 20:08:55 +0300

    Задача 2

--- Seminar1/Task2/Program.cs
+++ Seminar1/Task2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая на вход принимает два числа и 
+выдаёт, какое число большее, а какое меньшее.*/
+
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+int max = 0;
+if (num1 > num2)
+{
+    max = num1;
+}
+else
+{
+    max = num2;
+}
+
+Console.WriteLine($"Максимальное число: {max} ");
