12f2323f3d0303abfa0c1c61b361c394cb1c491a
Ogara <ya.rom4345@yandex.ru>
2022-09-20 22:19:28 +0300

    Задача 15

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -11,20 +11,30 @@
 
 /*Задача 13: Напишите программу, которая выводит третью цифру
 заданного числа или сообщает, что третьей цифры нет.*/
-Console.Write("Введите число: ");
-int number = int.Parse(Console.ReadLine());
+// Console.Write("Введите число: ");
+// int number = int.Parse(Console.ReadLine());
 
-if (number < 100) Console.Write("Третьей цифры нет");
-else if (number < 1000)
-    Console.Write($"Третья цифра этого числа: {number % 10}");
-else if (number < 10000)
-{
-    int num1 = number / 10;
-    Console.Write($"Третья цифра этого числа: {num1 % 10}");
-}
-else if (number < 100000)
-{
-    int num1 = number / 100;
-    Console.Write($"Третья цифра этого числа: {num1 % 10}");
-}
-else Console.Write("Число слишком большое");
+// if (number < 100) Console.Write("Третьей цифры нет");
+// else if (number < 1000)
+//     Console.Write($"Третья цифра этого числа: {number % 10}");
+// else if (number < 10000)
+// {
+//     int num1 = number / 10;
+//     Console.Write($"Третья цифра этого числа: {num1 % 10}");
+// }
+// else if (number < 100000)
+// {
+//     int num1 = number / 100;
+//     Console.Write($"Третья цифра этого числа: {num1 % 10}");
+// }
+// else Console.Write("Число слишком большое");
+
+/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую
+день недели, и проверяет, является ли этот день выходным.*/
+
+Console.Write("Введите цифру, обозначающую день недели: ");
+int day = int.Parse(Console.ReadLine());
+
+if (day >0 && day<6) Console.Write("Нет, этот день не выходной");
+else if (day >5 && day<8) Console.Write("Даа, этот день выходной!");
+else Console.Write("Нет такого дня недели");
