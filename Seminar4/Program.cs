697ed502bb7299ed4dfe2c90f66c18caab63c52e
Ogara <ya.rom4345@yandex.ru>
2022-09-27 07:22:04 +0300

    Задача 25

---
+++ Seminar4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+/*Задача 25: Напишите цикл, который принимает на вход два числа 
+(A и B) и возводит число A в натуральную степень B.*/
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write("Введите степень числа: ");
+int num = int.Parse(Console.ReadLine());
+
+int result = 1;
+for (int index = 1; index <=num; index++)
+{
+    result = result * number;
+}
+
+Console.WriteLine($"{number} в степени {num}: {result}");
