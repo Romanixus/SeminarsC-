d97b69ba2823a721466ff0e64ce665f26d63344c
Ogara <ya.rom4345@yandex.ru>
2022-09-27 22:07:02 +0300

    Задача 29

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -24,20 +24,44 @@
 /*Задача 27: Напишите программу, которая принимает 
 на вход число и выдаёт сумму цифр в числе.*/
 
-Console.Write("Введите число: ");
-string numstring = Console.ReadLine();
-int number = int.Parse(numstring);
-Console.WriteLine($"Сумма цифр в числе {number}: {GetSum(numstring.Length)}");
+// Console.Write("Введите число: ");
+// string numstring = Console.ReadLine();
+// int number = int.Parse(numstring);
+// Console.WriteLine($"Сумма цифр в числе {number}: {GetSum(numstring.Length)}");
 
-int GetSum(int limit)
+// int GetSum(int limit)
+// {
+//     int sum = 0;
+//     for (int index = 0; index < limit; index++)
+//     {
+//         int div = Convert.ToInt32(((Math.Pow(10, index))));
+//         int num = (number / div) % 10;
+//         //Console.WriteLine($"{number} / {div} % 10 = {num}");
+//         sum = sum + num;
+//     }
+//     return sum;
+// }
+
+/*Задача 29: Напишите программу, которая задаёт массив 
+из 8 рандомных элементов и выводит их на экран.*/
+
+int[] massiv = new int[8];
+FillArray(massiv);
+Console.Write("Восемь рандомных элементов массива: ");
+PrintArray(massiv);
+
+void FillArray(int[] array)
 {
-    int sum = 0;
-    for (int index = 0; index < limit; index++)
+    for (int index = 0; index < array.Length; index++)
     {
-        int div = Convert.ToInt32(((Math.Pow(10, index))));
-        int num = (number / div) % 10;
-        //Console.WriteLine($"{number} / {div} % 10 = {num}");
-        sum = sum + num;
+        array[index] = new Random().Next(-1000000, 1000000);
     }
-    return sum;
+}
+
+void PrintArray(int[] array)
+{
+    for (int index = 0; index < array.Length; index++)
+    {
+        Console.Write($"{array[index]} ");
+    }
 }
