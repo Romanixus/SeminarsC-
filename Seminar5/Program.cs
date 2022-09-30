e6eaaa26f1a8b608c0610838e8147591b808d84e
Ogara <ya.rom4345@yandex.ru>
2022-09-30 09:48:19 +0300

    Задача 36

--- Seminar5/Program.cs
+++ Seminar5/Program.cs
@@ -3,22 +3,64 @@
 /*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
 Напишите программу, которая покажет количество чётных чисел в массиве.*/
 
-int[] massiv = new int[1000];
+// int[] massiv = new int[1000];
+// FillArray(massiv);
+
+// Console.Write("Массив: ");
+// PrintArray(massiv);
+// Console.WriteLine();
+
+// Console.Write("Количество чётных элементов массива: ");
+// Counter(massiv);
+
+
+// void FillArray(int[] array)
+// {
+//     for (int index = 0; index < array.Length; index++)
+//     {
+//         array[index] = new Random().Next(100, 1000);
+//     }
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int index = 0; index < array.Length; index++)
+//     {
+//         Console.Write($"{array[index]} ");
+//     }
+// }
+
+// void Counter(int[] array)
+// {
+//     int count = 0;
+//     for (int index = 0; index < array.Length; index++)
+//     {
+//         if (array[index] % 2 == 0)
+//         {
+//             count = count + 1;
+//         }
+//     }
+//     Console.Write(count);
+// }
+
+/*Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.*/
+
+int[] massiv = new int [100];
 FillArray(massiv);
 
 Console.Write("Массив: ");
 PrintArray(massiv);
+
 Console.WriteLine();
-
-Console.Write("Количество чётных элементов массива: ");
-Counter(massiv);
-
+Console.Write("Сумма нечётных элементов массива: ");
+Summa(massiv);
 
 void FillArray(int[] array)
 {
     for (int index = 0; index < array.Length; index++)
     {
-        array[index] = new Random().Next(100, 1000);
+        array[index] = new Random().Next(-99, 100);
     }
 }
 
@@ -30,16 +72,14 @@ void PrintArray(int[] array)
     }
 }
 
-void Counter(int[] array)
+void Summa(int[] array)
 {
-    int count = 0;
-    for (int index = 0; index < array.Length; index++)
+    int sum = 0;
+    for (int index = 1; index < array.Length; index ++)
     {
-        if (array[index] % 2 == 0)
-        {
-            count = count + 1;
-        }
+        //Console.Write($"{array[index]} ");
+        sum = sum + array[index];
+        index++;
     }
-    Console.Write(count);
+    Console.Write(sum);
 }
-
