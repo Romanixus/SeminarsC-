cf23be37f8c25adae82fc5b7e87ed7b0ac2b9e5e
Ogara <ya.rom4345@yandex.ru>
2022-09-30 10:19:50 +0300

    Задача 38

--- Seminar5/Program.cs
+++ Seminar5/Program.cs
@@ -46,21 +46,62 @@
 /*Задача 36: Задайте одномерный массив, заполненный случайными числами. 
 Найдите сумму элементов, стоящих на нечётных позициях.*/
 
-int[] massiv = new int [100];
+// int[] massiv = new int [100];
+// FillArray(massiv);
+
+// Console.Write("Массив: ");
+// PrintArray(massiv);
+
+// Console.WriteLine();
+// Console.Write("Сумма нечётных элементов массива: ");
+// Summa(massiv);
+
+// void FillArray(int[] array)
+// {
+//     for (int index = 0; index < array.Length; index++)
+//     {
+//         array[index] = new Random().Next(-99, 100);
+//     }
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int index = 0; index < array.Length; index++)
+//     {
+//         Console.Write($"{array[index]} ");
+//     }
+// }
+
+// void Summa(int[] array)
+// {
+//     int sum = 0;
+//     for (int index = 1; index < array.Length; index ++)
+//     {
+//         //Console.Write($"{array[index]} ");
+//         sum = sum + array[index];
+//         index++;
+//     }
+//     Console.Write(sum);
+// }
+
+/*Задача 38: Задайте массив вещественных чисел. Найдите разницу 
+между максимальным и минимальным элементов массива.*/
+
+int[] massiv = new int[500];
 FillArray(massiv);
 
 Console.Write("Массив: ");
 PrintArray(massiv);
 
 Console.WriteLine();
-Console.Write("Сумма нечётных элементов массива: ");
-Summa(massiv);
+Console.Write("Разница между максимальным и минимальным элементом массива: ");
+Difference(massiv);
 
 void FillArray(int[] array)
 {
     for (int index = 0; index < array.Length; index++)
     {
-        array[index] = new Random().Next(-99, 100);
+        array[index] = new Random().Next(-9999, 10000);
     }
 }
 
@@ -72,14 +113,15 @@ void PrintArray(int[] array)
     }
 }
 
-void Summa(int[] array)
+void Difference(int[] array)
 {
-    int sum = 0;
-    for (int index = 1; index < array.Length; index ++)
+    int min = 0;
+    int max = 0;
+    for (int index = 0; index < array.Length; index++)
     {
-        //Console.Write($"{array[index]} ");
-        sum = sum + array[index];
-        index++;
+        if (array[index] > max) max = array[index];
+        if (array[index] < min) min = array[index];
     }
-    Console.Write(sum);
+    //Console.Write($"{max} - ({min}) = ");
+    Console.Write(max - min);
 }
