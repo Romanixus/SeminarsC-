36c89993dabebe275793e3be32e1e84376bd3d9b
Ogara <ya.rom4345@yandex.ru>
2022-10-07 11:48:59 +0300

    Задача 50

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -3,22 +3,53 @@
 /*Задача 47. Задайте двумерный массив размером m×n,
 заполненный случайными целыми числами.*/
 
-Console.Write("Количество строк: ");
-int rows = int.Parse(Console.ReadLine());
+// Console.Write("Количество строк: ");
+// int rows = int.Parse(Console.ReadLine());
 
-Console.Write("Количество столбцов: ");
-int columns = int.Parse(Console.ReadLine());
+// Console.Write("Количество столбцов: ");
+// int columns = int.Parse(Console.ReadLine());
 
-int[,] array = GetArray(rows, columns, -99, 100);
+// int[,] array = GetArray(rows, columns, -99, 100);
+// Console.WriteLine("Двумерный массив: ");
+// PrintArray(array);
+
+/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+и возвращает значение этого элемента или же указание, что такого элемента нет.*/
+
+int rows = 5;
+int columns = 10;
+Console.WriteLine($"Количество строк: {rows}");
+Console.WriteLine($"Количество столбцов: {columns}");
+
+int[,] array = GetArray(columns, rows, -99, 100);
 Console.WriteLine("Двумерный массив: ");
 PrintArray(array);
 
-int[,] GetArray(int m, int n, int minValue, int maxValue)
+for (int count = 0; count < 5; count++)
 {
-    int[,] array = new int[m, n];
-    for (int i = 0; i < m; i++)
+    Console.Write("Номер столбца: ");
+    int i = int.Parse(Console.ReadLine());
+
+    Console.Write("Номер строки: ");
+    int j = int.Parse(Console.ReadLine());
+
+    if (i >= columns || j >= rows)
     {
-        for (int j = 0; j < n; j++)
+        Console.WriteLine("Такого элемента нет");
+    }
+    else
+    {
+        Console.WriteLine($"Значение элемента: {array[i, j]}");
+    }
+}
+
+
+int[,] GetArray(int n, int m, int minValue, int maxValue)
+{
+    int[,] array = new int[n, m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
         {
 
             array[i, j] = new Random().Next(minValue, maxValue);
@@ -40,3 +71,4 @@ void PrintArray(int[,] array)
         Console.WriteLine();
     }
 }
+
