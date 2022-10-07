00b9b28d6cf002844d60a50f9b76844a732f61ca
Ogara <ya.rom4345@yandex.ru>
2022-10-07 14:11:45 +0300

    Задача 52

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -9,40 +9,64 @@
 // Console.Write("Количество столбцов: ");
 // int columns = int.Parse(Console.ReadLine());
 
-// int[,] array = GetArray(rows, columns, -99, 100);
+// int[,] array = GetArray(columns, rows, -99, 100);
 // Console.WriteLine("Двумерный массив: ");
 // PrintArray(array);
 
 /*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
 и возвращает значение этого элемента или же указание, что такого элемента нет.*/
 
-int rows = 5;
-int columns = 10;
+// int rows = 5;
+// int columns = 10;
+// Console.WriteLine($"Количество строк: {rows}");
+// Console.WriteLine($"Количество столбцов: {columns}");
+
+// int[,] array = GetArray(columns, rows, -99, 100);
+// Console.WriteLine("Двумерный массив: ");
+// PrintArray(array);
+
+// for (int count = 0; count < 5; count++)
+// {
+//     Console.Write("Номер столбца: ");
+//     int i = int.Parse(Console.ReadLine());
+
+//     Console.Write("Номер строки: ");
+//     int j = int.Parse(Console.ReadLine());
+
+//     if (i >= columns || j >= rows)
+//     {
+//         Console.WriteLine("Такого элемента нет");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Значение элемента: {array[i, j]}");
+//     }
+// }
+
+/*Задача 52. Задайте двумерный массив из целых чисел. Найдите 
+среднее арифметическое элементов в каждом столбце.*/
+
+int rows = 40;
+int columns = 15;
 Console.WriteLine($"Количество строк: {rows}");
 Console.WriteLine($"Количество столбцов: {columns}");
 
-int[,] array = GetArray(columns, rows, -99, 100);
+int[,] array = GetArray(columns, rows, -9, 10);
 Console.WriteLine("Двумерный массив: ");
 PrintArray(array);
 
-for (int count = 0; count < 5; count++)
+Console.Write("Среднее арифметическое в каждом столбце соответственно:");
+for (int i = 0; i < array.GetLength(0); i++)
 {
-    Console.Write("Номер столбца: ");
-    int i = int.Parse(Console.ReadLine());
-
-    Console.Write("Номер строки: ");
-    int j = int.Parse(Console.ReadLine());
-
-    if (i >= columns || j >= rows)
+    double sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
     {
-        Console.WriteLine("Такого элемента нет");
+        sum = array[i, j] + sum;
     }
-    else
-    {
-        Console.WriteLine($"Значение элемента: {array[i, j]}");
-    }
+    Console.SetCursorPosition(i * 4, rows + 4);
+    if (sum >= 0) Console.Write(" ");
+    Console.Write(sum / rows);
 }
-
 
 int[,] GetArray(int n, int m, int minValue, int maxValue)
 {
