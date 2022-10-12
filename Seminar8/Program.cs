6ceac001b6640e9f24ea2a937b441762e7581569
Ogara <ya.rom4345@yandex.ru>
2022-10-12 18:00:05 +0300

    Задача 56

--- Seminar8/Program.cs
+++ Seminar8/Program.cs
@@ -9,34 +9,62 @@ PrintArray(array);
 /*Задача 54: Задайте двумерный массив. Напишите программу, которая 
 упорядочит по убыванию элементы каждой строки двумерного массива.*/
 
-Console.WriteLine("Упорядоченные элементы: ");
-int minValue = -999999; //Минимальное число в массиве (меньше можно, больше нельзя)
-int[,] minValues = (int[,])array.Clone(); //Копия массива, в которой все элементы будут minValue
-DecreasingArray(minValues); 
+// Console.WriteLine("Упорядоченные элементы: ");
+// int minValue = -999999; //Минимальное число в массиве (меньше можно, больше нельзя)
+// int[,] minValues = (int[,])array.Clone(); //Копия массива, в которой все элементы будут minValue
+// DecreasingArray(minValues); 
 
-void DecreasingArray(int[,] minValues) 
+// void DecreasingArray(int[,] minValues) 
+// {
+//     for (int row = 0; row < minValues.GetLength(0); row++) //Переход на следующий ряд
+//     {
+//         for (int j = 0; j < minValues.GetLength(1); j++)
+//         {
+//             int max = minValue;
+//             int index = 0;
+//             for (int i = 0; i < minValues.GetLength(0); i++)
+//             {
+//                 if (minValues[i, j] > max)
+//                 {
+//                     max = minValues[i, j];
+//                     index = i;
+//                 }
+//             }
+//             Console.SetCursorPosition(row * distanceBetweenColumns, j + minValues.GetLength(1) + indentArray * 2);
+//             if (max >= 0) Console.Write(" ");
+//             Console.WriteLine(max);
+//             minValues[index, j] = minValue; // Уменьшение максимального элемента в ряду
+//         }
+//     }
+// }
+
+/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+которая будет находить строку с наименьшей суммой элементов.*/
+
+int minSum = 0; //Значение должно быть больше, чем у строки с наименьшей суммой элементов
+int minSumRow = 0;
+
+for (int j = 0; j < array.GetLength(1); j++)
 {
-    for (int row = 0; row < minValues.GetLength(0); row++) //Переход на следующий ряд
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < minValues.GetLength(1); j++)
-        {
-            int max = minValue;
-            int index = 0;
-            for (int i = 0; i < minValues.GetLength(0); i++)
-            {
-                if (minValues[i, j] > max)
-                {
-                    max = minValues[i, j];
-                    index = i;
-                }
-            }
-            Console.SetCursorPosition(row * distanceBetweenColumns, j + minValues.GetLength(1) + indentArray * 2);
-            if (max >= 0) Console.Write(" ");
-            Console.WriteLine(max);
-            minValues[index, j] = minValue; // Уменьшение максимального элемента в ряду
-        }
+        sum = sum + array[i, j]; //Складываем все элементы строки
+    }
+    Console.SetCursorPosition(array.GetLength(0) * distanceBetweenColumns, j + indentArray);
+    Console.Write("|");
+    if (sum >= 0) Console.Write(" ");
+    Console.WriteLine(sum);
+    if (sum < minSum) //Находим строку с наименьшей суммой элементов
+    {
+        minSum = sum;
+        minSumRow = j;
     }
 }
+
+Console.WriteLine($"Номер строки с наименьшей суммой элементов ({minSum}): {minSumRow}*");
+Console.Write("*строки номируются с нуля");
+
 
 int[,] GetArray(int columns, int rows, int minValue, int maxValue)
 {
